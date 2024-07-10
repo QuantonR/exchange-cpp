21dbad1238c89e53314c5e9658848cd4e26d81c8
QuantonR <canton.r998@gmail.com>
2024-07-10 22:03:28 +0200

    clean up

--- Exchange.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  Exchange.cpp
-//  exchange_cpp
-//
-//  Created by Riccardo Canton on 07/07/24.
-//
-
-#include "Exchange.hpp"
--- LOBSide.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  LOBSide.cpp
-//  exchange_cpp
-//
-//  Created by Riccardo Canton on 08/06/24.
-//
-
-#include "LOBSide.hpp"
